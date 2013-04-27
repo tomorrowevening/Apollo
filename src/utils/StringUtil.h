8cf26847df5f9246af79abfcf62d158f18008b66
Colin Duffy <colin.duffy@landeu-nmx3530.deutschinc.net>
2013-04-27 16:44:20 -0700

    Minor changes

--- src/utils/StringUtil.h
+++ src/utils/StringUtil.h
@@ -1,36 +1,42 @@
 //
 //  StringUtil.h
 //
-//  Created by Colin Duffy on 9/27/12.
+//  Created by Colin Duffy on 4/27/13.
 //
 //
 
 #pragma once
 
-#include "ofUtils.h"
+// String
+#include "Apollo.h"
+#include <iostream>
+#include <iostream>
+#include <sstream>
 
-class StringUtil {
-public:
+namespace Apollo {
 	
-	static string fileType(string path) {
-		return path.substr(path.find_last_of(".")+1);
+	using std::ostringstream;
+	using std::stringstream;
+	
+	template <class T>
+	string toString(const T& value) {
+		ostringstream out;
+		out << value;
+		return out.str();
 	}
 	
-	static string formatDigits(int num, int totalDigits, string prefix, string suffix) {
-		int i, n;
-		int hundreds = 1;
-		string result = "";
-		for(i = 0; i <= totalDigits; ++i) {
-			hundreds *= 10;
-			if(num < hundreds) {
-				for(n = i; n < totalDigits-1; ++n) {
-					result += "0";
-				}
-				result += ofToString(num);
-				break;
+	template<class T>
+	string toString(const vector<T>& values) {
+		stringstream out;
+		int n = values.size();
+		out << "{";
+		if(n > 0) {
+			for(int i = 0; i < n - 1; i++) {
+				out << values[i] << ", ";
 			}
+			out << values[n - 1];
 		}
-		
-		return prefix + result + suffix;
+		out << "}";
+		return out.str();
 	}
-};
+}
