c79a9c34b2e3b5886029ca1411a8e50a0480846b
Colin Duffy <colin.duffy@landeu-nmx3530.deutschinc.net>
2013-04-27 16:44:05 -0700

    Apollo macros added

--- src/Apollo.h
+++ src/Apollo.h
@@ -7,10 +7,6 @@
 
 #pragma once
 
-namespace Apollo {
-	
-}
-
 // Common
 #include <float.h>
 #include <string>
@@ -19,10 +15,37 @@ namespace Apollo {
 #include <list>
 #include <map>
 #include <vector>
+#include <iterator>
 
-// Math
+#define APOLLO_VERSION 250
+
+namespace Apollo {
+	
+	using std::list;
+	using std::map;
+	using std::string;
+	using std::vector;
+	using std::iterator;
+	
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+	#define APOLLO_MSW
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+	#define APOLLO_LINUX
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+	#define APOLLO_COCOA
+	#if TARGET_OS_IPHONE
+		#define APOLLO_COCOA_TOUCH
+		#if TARGET_IPHONE_SIMULATOR
+			#define APOLLO_COCOA_TOUCH_SIMULATOR
+		#else
+			#define APOLLO_COCOA_TOUCH_DEVICE
+		#endif
+	#else
+		#define APOLLO_MAC
+	#endif
+#endif
+	
+}
+
 #include "MathUtil.h"
-#include "Vec.h"
-
-// Events
-#include "Events.h"
+#include "StringUtil.h"
