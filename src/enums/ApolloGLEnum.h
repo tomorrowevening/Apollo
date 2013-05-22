c593e97576a52dbe9c6528fd80fc21d813040b14
Colin Duffy <colin@tomorrowevening.com>
2013-05-21 21:52:11 -0700

    OpenGL includes

--- src/enums/ApolloGLEnum.h
+++ src/enums/ApolloGLEnum.h
@@ -6,7 +6,41 @@
 //
 
 #pragma once
-#include "Apollo.h"
+#include "ApolloCore.h"
+
+#ifdef APOLLO_MAC
+#include "GL/glew.h"
+#include <OpenGL/gl.h>
+#endif
+
+#ifdef APOLLO_MSW
+#include <windows.h>
+#include "GL\glew.h"
+#include "GL\wglew.h"
+#include "glu.h"
+#endif
+
+#ifdef APOLLO_LINUX
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#endif
+
+#ifdef APOLLO_COCOA_TOUCH
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#endif
+
+#ifdef APOLL_ANDROID
+#include <GLES/gl.h>
+#include <GLES/glext.h>
+#endif
+
+#include "ApolloFBO.h"
+#include "ApolloShader.h"
+#include "ApolloTexture.h"
 
 namespace Apollo {
 	namespace gl {
