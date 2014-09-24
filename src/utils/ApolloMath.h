604670174f93693d6e13dfa7158905889473e088
Colin <colin@tomorrowevening.com>
2014-09-23 22:55:53 -0700

    Sine Range animation

--- src/utils/ApolloMath.h
+++ src/utils/ApolloMath.h
@@ -60,5 +60,6 @@ namespace Apollo {
 	float   distance(float x1, float y1, float z1, float x2, float y2, float z2);
 	
 	float cosRange(float degrees, float range, float min);
+	float sinRange(float degrees, float range, float min);
     
 }
